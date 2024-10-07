ab9b8356fc9943e985df61c993b7d32e9d92a2db
Gwizee <risingfire69@gmail.com>
2024-10-07 21:37:19 +0600

    Grater than Less than Activity has completed

---
+++ main.py
@@ -0,0 +1,10 @@
+number = int(input("Enter a Number : "))
+
+if number > 15 :
+    print(number, "is greater than 15")
+    print(" and I am inside the if block")
+else :
+    print(number, "is less than 15")
+    print(" and I am inside the else block")
+
+print("I am outside th if/else block")
